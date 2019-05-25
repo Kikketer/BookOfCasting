f22d38e4ebe395e928e61b23dee609b394ca6b13
Chris Weed <chris@cjweed.com>
2019-05-24 22:49:43 -0500

    Updated phrases

--- server/phrases.js
+++ server/phrases.js
@@ -3,99 +3,156 @@ module.exports = {
   alohomora: [
     'A door has unlocked',
     '${name} has unlocked a door',
-    'Great! ${name} you can now cast alohomora',
     'Try this on a lock ${name}',
-    'Did you hear that click ${name}?'
+    'Did you hear that click ${name}?',
+    'Check the door ${name}'
   ],
   aparecum: [
     'I have nothing to hide ${name}',
-    'Try this spell on a parchment ${name}'
+    'Try this spell on a parchment ${name}',
+    'It has worked if you can read this',
+    'I hold secrets, you are revealing them'
   ],
   ascendio: [
+    'You are not powerful enough to lift yourself',
+    'Try jumping while casting ${name}',
+    'Ascendio works best underwater ${name}',
+    'Use this to escape danger ${name}'
+  ],
+  descendo: [
+    'Try aiming at a bird ${name}',
+    'Are there witches flying ${name}?',
+    'You can now pull things down ${name}',
+    'Something fell ${name}'
+  ],
+  incendio: [
     'We don\'t want a fire here ${name}',
-    'Stop, that\'s dangerious ${name}',
-    'I suggest you cool off ${name}'
+    'Stop, that\'s dangerous ${name}',
+    'I suggest you cool off ${name}',
+    'A fire has started somewhere',
+    'Careful with that ${name}',
+    'Let\'s not burn the house down ${name}',
+    'Use this only in defense ${name}',
+    'Handy when you get cold ${name}',
+    'Grab the s\'mores ${name}'
   ],
   wingardium: [
     'Careful what you lift ${name}',
     'I\'m too heavy to lift ${name}',
-    'Try Wingardium on lighter items ${name}'
+    'Try Wingardium on lighter items ${name}',
+    'Feathers work best ${name}',
   ],
-  descendo: [
-    'There are no stairs here ${name}',
-    'A stairway has been revealed ${name}',
-    'Careful climbing down ${name}'
+  mimble: [
+    'Try this on a friend ${name}',
+    'Mmmpha ammmbpp',
+    'Only master wizards may cast this'
   ],
-  mimble: [],
   silencio: [
-    'Attempt this on your fellow wizards!',
+    'Attempt this on your fellow wizards, ${name}!',
     'I am unable to speak ${name}',
     'I have never spoken ${name}',
     'Beware of silencing others ${name}',
     'mmmph mmmph mmmph'
   ],
   specialis: [
-    'I have a wire coming out my back',
-    'I can not see you',
-    'I have no eyes',
-    'I may be controlled by other wizards'
+    'I have had many spells cast upon me ${name}',
+    'There are too many spells to list ${name}',
+    'My purpose is to be cast upon ${name}',
+    'You have much to learn ${name}'
   ],
   tarantaliegra: [
     'I have no legs ${name}',
     'Someone is dancing ${name}',
-    'No music needed to dance ${name}'
+    'No music needed to dance ${name}',
+    'Do you hear someone dancing?'
   ],
-  regaro: [],
-  arresto:[],
+  reparo: [
+    'You\'ve fixed my glasses ${name}',
+    'Find a broken glass ${name}',
+    'I\'m not broken ${name}',
+    'That fixed it ${name}'
+  ],
+  riddikulus: [
+    'You must imagine your fear ${name}',
+    'I am not a boggart ${name}',
+    'Find a boggart ${name}',
+    'Something is odd in the other room'
+  ],
+  expecto: [
+    'Practice this for later ${name}',
+    'You may need this spell soon ${name}',
+    'Have your friends try this as well',
+    'You are ready to defend yourself'
+  ],
+  arresto:[
+    'I\'m not falling ${name}',
+    'I don\'t move ${name}',
+    'You have slowed the fall of an object',
+    'Find the object you slowed ${name}'
+  ],
   finiteincantatem: [
-    'You have stopped a spell ${name}',
-    'My spells have been blocked ${name}'
+    'You have reversed a spell ${name}',
+    'I have no spell upon me ${name}',
+    'You have undone a wicked spell ${name}',
+    'Remember this to help your friends ${name}'
   ],
   nox: [
     'A light has gone out ${name}',
     'That\'ll make your room dark ${name}',
     'Good use of darkening the room ${name}',
-    'Beware of the darkness ${name}',
-    'You perfected nox ${name}'
+    'Beware of the darkness ${name}'
   ],
   lumos: [
     'I can now see, ${name}',
     'You can now light rooms ${name}!',
     'You have mastered lumos ${name}'
   ],
-  herbivicus: [],
+  herbivicus: [
+    'Try this on a plant, not a book',
+    'A plant is growing wildly ${name}',
+    'Try this in the garden ${name}',
+    'Not all plants grow quickly ${name}'
+  ],
   locomotor: [
     'Objects are moving, ${name}',
     'I feel alive ${name}',
-    'What was broken now functions'
+    'I\'m a bit heavy to move ${name}',
+    'Try this on small items ${name}'
   ],
-  flabilis: [],
   dancing: [
-    'There are no skeletons here ${name}'
+    'There are no skeletons to dance',
+    'Hear the clacking dance ${name}'
   ],
-  revelio: [],
+  revelio: [
+    'I have nothing to reveal ${name}',
+    'Look about, you may find secrets ${name}',
+    'I may have secrets ${name}',
+    'Secrets are meant to be secret ${name}'
+  ],
   aquamenti: [
     'Let\'s not make a mess ${name}',
     'Use this on a hot day ${name}',
-    'Your wand is now spraying water ${name}'
+    'Your wand is now spraying water ${name}',
+    'The sink turned on ${name}',
+    'Are you thirsty ${name}?'
   ],
   stuipify: [
     'Good work disarming your enemy ${name}',
     'The opponent drops their wand, ${name}',
-    'Use stupify only in emergencies ${name}',
-    'You have mastered stupify ${name}'
+    'Use stupify only in emergencies ${name}'
   ],
   oppugno: [
     'You have no items conjured ${name}',
     'Beware the power of attack ${name}',
-    'Are you sure you should cast this ${name}'
+    'Are you sure you should cast this ${name}?',
+    'This is a powerful spell ${name}'
   ],
-  meto: [],
-  incendio: [
-    'Let\'s not burn the house down ${name}',
-    'Use this only in defense ${name}',
-    'Handy when you get cold ${name}',
-    'Grab the s\'mores ${name}'
+  metelojinx: [
+    'We have had enough rain ${name}',
+    'You are inside ${name}',
+    'Listen for the rain ${name}',
+    'I don\'t like being wet ${name}',
+    'Try this in winter ${name}'
   ],
   expelliarmus: [
     'Wow, ${name} you disarmed a wizard',
@@ -132,14 +189,5 @@ module.exports = {
     'Stop making things up, that\'s dangerous'
   ],
   generic: [
-    'Close, but try again ${name}',
-    'You are a powerful wizard ${name}',
-    'Try again ${name}, that was close',
-    'You are getting better ${name}',
-    'Only use this spell for good ${name}',
-    'Your skills have improved ${name}',
-    'Remember this spell ${name}',
-    'Try again with more feeling ${name}',
-    'You are truely powerful ${name}'
   ]
 }
